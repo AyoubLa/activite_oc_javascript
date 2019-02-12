ec7c562f4920da8daca4e27f8145a20591547e47
unknown <SQLI@SQLI-ALAHBALI.SQLI>
2019-02-12 15:40:11 +0100

    complétion de la partie html/js pour répondre à lexercice

--- js/contacts.js
+++ js/contacts.js
@@ -4,6 +4,16 @@ Activité : gestion des contacts
 
 // TODO : complétez le programme
 
+let lstContacts = document.getElementById('lstContacts');
+let ajouter = document.getElementById('ajouter');
+let formulaire = document.getElementById('formulaire');
+let quitter = document.getElementById('quitter');
+let submitAjout = document.getElementById('submitAjout');
+let nom = document.getElementById('nom');
+let prenom = document.getElementById('prenom');
+let contacts = document.getElementById('contacts');
+
+// Classes ---------------------------------
 class Contact{
 	constructor(nom, prenom){
 		this.nom = nom;
@@ -20,18 +30,57 @@ class ListeDeContacts{
 	}
 }
 
+
+var contact = new Contact("","");
 let listeContacts = new ListeDeContacts(new Contact("Lévisse","Carole"));
 listeContacts.ajouterContact(new Contact("Nelsonne","Mélodie"));
 
-let lstContacts = document.getElementById('lstContacts');
-let quitter = document.getElementById('quitter');
+
+// Events -----------------------------------
+nom.addEventListener('change', function(event){
+	contact.nom = event.target.value;
+});
+prenom.addEventListener('change', function(event){
+	contact.prenom = event.target.value;
+});
 
 lstContacts.addEventListener('click', function() {
-
+	
+	formulaire.style.display = "none";
+	while(contacts.firstChild){
+		contacts.removeChild(contacts.firstChild);
+	}
 	for (let i = 0; i < listeContacts.contacts.length; i++) {
-		// console.log(listeContacts.contacts.length);
-		console.log(listeContacts.contacts[i].nom+' '+ listeContacts.contacts[i].prenom);
+
+		let contactElement = document.createElement('div');
+		let textContact = document.createTextNode(listeContacts.contacts[i].nom+' '+ listeContacts.contacts[i].prenom);
+		contactElement.setAttribute("class","contact");
+		contactElement.appendChild(textContact);
+		contacts.appendChild(contactElement);
 		
 		
 	}
+});
+
+ajouter.addEventListener('click', function(){
+
+	formulaire.style.display = "block";
+});
+
+submitAjout.addEventListener('click', function(event){
+	event.preventDefault();
+	if(contact.nom !== "" && contact.prenom !== ""){
+		listeContacts.ajouterContact(contact);
+		let contactElement = document.createElement('div');
+		let textContact = document.createTextNode(contact.nom+' '+ contact.prenom);
+		contactElement.setAttribute("class","contact");
+		contactElement.appendChild(textContact);
+		contacts.appendChild(contactElement);
+	}else{
+		alert("vous devez remplire tous les champs avant de confirmer l'ajout")
+	}
+	contact = new Contact("","");
+});
+quitter.addEventListener('click', function(){
+	location.reload();
 });
